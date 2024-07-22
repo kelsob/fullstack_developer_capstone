a22188d75f5f35faa31f1b6d0948f97b8178e804
kelsob <102971760+kelsob@users.noreply.github.com>
2024-07-22 14:31:31 -0400

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,7 +16,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True,
+                                           serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
             ],
@@ -24,12 +25,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cartype', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('COUPE', 'Coupe'), ('HATCHBACK', 'Hatchback'), ('STATIONWAGON', 'Station Wagon'), ('TRUCK', 'Truck'), ('VAN', 'Van'), ('CONVERTIBLE', 'Convertible'), ('CROSSOVER', 'Crossover')], default='SEDAN', max_length=12)),
+                ('id', models.BigAutoField(auto_created=True,
+                                           primary_key=True, serialize=False, verbose_name='ID')),
+                ('cartype', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'),
+                                                      ('COUPE', 'Coupe'), ('HATCHBACK', 'Hatchback'), ('STATIONWAGON', 'Station Wagon'),
+                                                      ('TRUCK', 'Truck'), ('VAN', 'Van'), ('CONVERTIBLE', 'Convertible'), ('CROSSOVER', 'Crossover')],
+                                             default='SEDAN', max_length=12)),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
                 ('dealerid', models.IntegerField()),
-                ('year', models.IntegerField(default=2000, validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(2025)])),
+                ('year', models.IntegerField(default=2000, validators=[django.core.validators.MinValueValidator(1),
+                                                                       django.core.validators.MaxValueValidator(2025)])),
                 ('carmake', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
             ],
         ),
