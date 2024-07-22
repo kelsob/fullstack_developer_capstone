de07c8981eab08c68546a69edefd0e2ac9702211
kelsob <102971760+kelsob@users.noreply.github.com>
2024-07-22 14:34:22 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -9,6 +9,8 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
+
 class CarModel(models.Model):
     SEDAN = "SEDAN"
     SUV = "SUV"
@@ -26,11 +28,11 @@ class CarModel(models.Model):
         ("TRUCK", "Truck"),
         ("VAN", "Van"),
         ("CONVERTIBLE", "Convertible"),
-        ("CROSSOVER", "Crossover",)
-    )
+        ("CROSSOVER", "Crossover")
+        )
     cartype = models.CharField(max_length=12,
-                            choices = TYPES,
-                            default = "SEDAN")
+                            choices=TYPES,
+                            default="SEDAN")
 
     name = models.CharField(max_length=100)
     description = models.TextField()
