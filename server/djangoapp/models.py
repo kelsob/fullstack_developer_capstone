65f4df9b23a42c01a080e5fdbbf4fe774487e379
kelsob <102971760+kelsob@users.noreply.github.com>
2024-07-22 14:42:16 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -31,14 +31,15 @@ class CarModel(models.Model):
         ("CROSSOVER", "Crossover")
         )
     cartype = models.CharField(max_length=12,
-                            choices=TYPES,
-                            default="SEDAN")
+                                choices=TYPES,
+                                default="SEDAN")
 
     name = models.CharField(max_length=100)
     description = models.TextField()
     carmake = models.ForeignKey("CarMake", on_delete=models.CASCADE)
     dealerid = models.IntegerField()
-    year = models.IntegerField(default=2000, validators=[MinValueValidator(1), MaxValueValidator(2025)])
+    year = models.IntegerField(default=2000,
+        validators=[MinValueValidator(1), MaxValueValidator(2025)])
 
     def __str__(self):
         return self.name
